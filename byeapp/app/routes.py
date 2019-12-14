7e1efa2f55dfd7b31569fa368f2b422ac55692d2
chrisandoryan <chrisando.siahaan@gmail.com>
2019-12-14 14:32:22 +0700

    added domain extraction

--- byeapp/app/routes.py
+++ byeapp/app/routes.py
@@ -4,17 +4,15 @@ from app import app
 from pyvirtualdisplay import Display
 import socket
 import re
+from urllib.parse import urlparse
 import time
 
 FLAG = "SG9ob2hvLiBIYXBweSBIb2xpZGF5Lg=="
 
 def valid_target(url):
     ip = ""
-    if url.startswith('http'):
-        url = re.sub(r'https?:\\', '', url)
-    if url.startswith('www.'):
-        url = re.sub(r'www.', '', url)
     try:
+        url = urlparse(url).netloc
         ip = socket.gethostbyname(url)
     except Exception as e:
         print(e)
